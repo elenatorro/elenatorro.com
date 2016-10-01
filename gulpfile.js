47c97c2b156bc8a85cf3415bf2bd6d114166392d
Elena Torro <elenatorro@MacBook-Pro-de-Elena.local>
2016-10-01 19:32:22 +0200

    total reset

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+var
+  gulp = require('gulp'),
+  babel = require('gulp-babel'),
+  del = require('del'),
+  vinyl = require('vinyl-paths'),
+  browserSync = require('browser-sync'),
+  sass = require('gulp-sass'),
+  prefix = require('gulp-autoprefixer'),
+  minifycss = require('gulp-minify-css'),
+  svgmin = require('gulp-svgmin');
+
+
+const paths = {
+    baseDir: './',
+    sourceHTML: 'index.html',
+    sourceJS: './src/**/*.js',
+    sourceSCSS: './src/styles/main.scss',
+    watchSCSS: ['./src/styles/*.scss'],
+    srcImages: './src/images/**',
+    srcFonts: './bower_components/allineed/src/fonts/**/*',
+    dest: './',
+    destImages: './dist/assets/images/',
+    destFonts: './dist/assets/fonts/',
+    destCSS: './dist/assets/css/'
+  },
+  PRESETS = ['es2015'],
+  SOURCES = [paths.sourceHTML],
+  OUTPUT_STYLE = 'expanded',
+  CLEAN_READ = false,
+  BABEL_STEAM = true;
+
+
+gulp.task('browserSync', function () {
+  browserSync({
+    server: {
+      baseDir: paths.baseDir
+    }
+  })
+})
+
+gulp.task('dist', function () {
+  return gulp.src(SOURCES)
+    .pipe(gulp.dest(paths.dest))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+})
+
+gulp.task('watch', function () {
+  gulp.watch(paths.sourceHTML, ['dist'])
+  gulp.watch(paths.sourceJS, ['babel'])
+  gulp.watch(paths.watchSCSS, ['sass'])
+})
+
+gulp.task('clean', function () {
+  return gulp.src(paths.dest, { read: CLEAN_READ })
+    .pipe(vinyl(del))
+})
+
+gulp.task('fonts', function () {
+  return gulp.src(paths.srcFonts)
+    .pipe(gulp.dest(paths.destFonts));
+})
+
+gulp.task('images', function () {
+  return gulp.src(paths.srcImages)
+    .pipe(gulp.dest(paths.destImages));
+})
+
+gulp.task('sass', function () {
+  gulp.src(paths.sourceSCSS)
+    .pipe(sass({
+      outputStyle: OUTPUT_STYLE
+    }))
+    .pipe(gulp.dest(paths.destCSS))
+    .pipe(minifycss())
+    .pipe(gulp.dest(paths.destCSS))
+})
+
+
+gulp.task('default', ['dist', 'browserSync', 'sass', 'fonts', 'images', 'watch'])
+gulp.task('compile', ['dist', 'sass', 'fonts', 'images'])
